f30731c1181a6e5d2b5a3953042f8f7700481608
kevinwdt <121691076+kevinwdt@users.noreply.github.com>
2022-12-31 14:34:02 -0500

    Create Blocks.java

---
+++ Blocks.java
@@ -0,0 +1,11 @@
+class BlockDemo {
+    public static void main(String[] args) {
+        boolean condition = true;
+        if (condition) { // Begin block 1
+            System.out.println("Condition is true");
+        } // End Block 1
+        else { // Begin block 2
+            System.out.println("Condition is false");
+        } // End Block 2
+    }
+}
